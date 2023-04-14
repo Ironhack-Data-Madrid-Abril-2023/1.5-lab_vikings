d2d520d8f0fb275009dceeaadefed6f9a1d4b58c
gusavato <augusto.abad@outloock.com>
2023-04-14 13:54:34 +0200

    primer commit

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -2,8 +2,29 @@
 # Soldier
 
 
+# class Soldier:
+
+#     def init(self, health, strength):
+#         self.health=health
+#         self.strength=strength
+
+#     def attack(self):
+#         return self.strength
+
+#     def receiveDamage(self, damage):
+#         self.health-=damage
+
 class Soldier:
-    pass
+    
+    def __init__(self, health, strength) -> None:
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self,damage):
+        self.health -= damage
 
 # Viking
 
