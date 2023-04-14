88bdd67526fdacc36db4c57d4d7d5e3894041e57
Carlos Palomares <carlospalomaresgarcia@gmail.com>
2023-04-14 18:34:22 +0200

    [Carlos Palomares] (1.5-lab_vikings)

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,9 +1,4 @@
 
-''' En la clase war, no consigo quitar el error en las funciones de ataque... IndexError: list index out of range
-El error parece que viene produce porque las listas vacias que he creado de los dos ejercitos (self.vikingArmy y self.saxonArmy)
-estan vacías, y la funcion random no se puede aplicar a una lista vacia'''
-# Entiendo el error, pero no se como arreglarlo
-
 import random # For the war random choice
 
 class Soldier:
@@ -66,24 +61,26 @@ class War:
         self.saxonArmy.append(Saxon)
     
     def vikingAttack(self):
-        random_viking = random.choice(self.vikingArmy)
-        random_saxon = random.choice(self.saxonArmy)
-        random_saxon.receiveDamage(random_viking.attack())
-        if random_saxon.health <= 0:
-            self.saxonArmy.remove(random_saxon)
+        Viking = random.choice(self.vikingArmy)
+        Saxon = random.choice(self.saxonArmy)
+        Saxon.receiveDamage(Viking.strength)
+        if Saxon.health <= 0:
+            self.saxonArmy.remove(Saxon)
             return "A Saxon has died in combat"
         else:
-            return Saxon.receiveDamage(random_viking.attack())
+            return Saxon.receiveDamage(Viking.strength)
 
     def saxonAttack(self):
-        random_viking = random.choice(self.vikingArmy)
-        random_saxon = random.choice(self.saxonArmy)
-        random_viking.receiveDamage(random_saxon.attack())
-        if random_viking.health <= 0:
-            self.vikingArmy.remove(random_viking)
-            return f"R.I.P. {random_viking.name}"
+        Viking = random.choice(self.vikingArmy)
+        Saxon = random.choice(self.saxonArmy)
+        Viking.receiveDamage(Saxon.strength)
+        if Viking.health <= 0:
+            self.vikingArmy.remove(Viking)
+            return f"{Viking.name} has died in act of combat"
         else:
-            return random_viking.receiveDamage(random_saxon.attack())
+            return f"{Viking.name} has received {Saxon.strength} points of damage"
+        
+        
     def showStatus(self):
         if self.saxonArmy == []:
             return "Vikings have won the war of the century!"
