3bfacb7c20d03508d2a95228d93330a60fec3b84
gusavato <augusto.abad@outloock.com>
2023-04-17 09:12:33 +0200

    septimo commit. progresando con el bonus

--- file.py
+++ file.py
@@ -23,56 +23,57 @@ sera la encargada de:
                 
     """
 
+
 class VikingArmy:
-    VIK_NAMES = ['Ragnar', 'Ivar', 'Bjorn', 'Harald', 'Erik', 'Leif', 'Sigurd',
-                'Olaf', 'Thorstein', 'Hakon', 'Gunnar', 'Asgeir', 'Sten', 
-                'Knut', 'Sven', 'Erling', 'Magnus', 'Finn', 'Vidar', 
-                'Hans', 'Orvar', 'Gudmund', 'Bard', 'Eirik', 'Gudbrand', 
-                'Ole', 'Birger', 'Tore', 'Arne', 'Halvor', 'Trygve', 'Gjermund',
-                'Jens', 'Rolf', 'Lars', 'Odd', 'Sindre', 'Peder', 'Vegard',
-                'Herman', 'Johannes', 'Einar', 'Nils', 'Per', 'Geir', 'Karl', 
-                'Dag', 'Ludvig', 'Ola', 'Kjell', 'Stian', 'Inge', 'Anders', 
-                'Øyvind', 'Morten', 'Joakim', 'Kåre', 'Pål', 'Håkon', 'Åge', 
-                'Knut', 'Jan', 'Even', 'Terje', 'Erlend', 'Lasse', 'Tommy', 
-                'Børre', 'Arvid', 'Jon', 'Espen', 'Fredrik', 'Marius', 'Simen', 
-                'Sondre', 'Christian', 'Erik', 'Hans', 'Kai', 'Kristian', 
-                'Steffen', 'Svein', 'Torbjørn', 'Trond', 'Vidar', 'Wilhelm', 
-                'Yngve', 'Øivind', 'Bjarte', 'David', 'Eivind', 'Gard', 'Ivar', 
-                'Jørgen', 'Kevin', 'Magnar', 'Ole', 'Petter']
-    
+
     def __init__(self, n_vikings) -> None:
+        self.VIK_NAMES = ['Ragnar', 'Ivar', 'Bjorn', 'Harald', 'Erik', 'Leif', 'Sigurd',
+                          'Olaf', 'Thorstein', 'Hakon', 'Gunnar', 'Asgeir', 'Sten',
+                          'Knut', 'Sven', 'Erling', 'Magnus', 'Finn', 'Vidar',
+                          'Hans', 'Orvar', 'Gudmund', 'Bard', 'Eirik', 'Gudbrand',
+                          'Ole', 'Birger', 'Tore', 'Arne', 'Halvor', 'Trygve', 'Gjermund',
+                          'Jens', 'Rolf', 'Lars', 'Odd', 'Sindre', 'Peder', 'Vegard',
+                          'Herman', 'Johannes', 'Einar', 'Nils', 'Per', 'Geir', 'Karl',
+                          'Dag', 'Ludvig', 'Ola', 'Kjell', 'Stian', 'Inge', 'Anders',
+                          'Øyvind', 'Morten', 'Joakim', 'Kåre', 'Pål', 'Håkon', 'Åge',
+                          'Knut', 'Jan', 'Even', 'Terje', 'Erlend', 'Lasse', 'Tommy',
+                          'Børre', 'Arvid', 'Jon', 'Espen', 'Fredrik', 'Marius', 'Simen',
+                          'Sondre', 'Christian', 'Erik', 'Hans', 'Kai', 'Kristian',
+                          'Steffen', 'Svein', 'Torbjørn', 'Trond', 'Vidar', 'Wilhelm',
+                          'Yngve', 'Øivind', 'Bjarte', 'David', 'Eivind', 'Gard', 'Ivar',
+                          'Jørgen', 'Kevin', 'Magnar', 'Ole', 'Petter']
         self.n_vikings = n_vikings
         self.army = []
 
     def create_army(self) -> list:
-        global VIK_NAMES
         name = ''
-        for _ in range(len(self.n_vikings)):
-            name = random.choice(VIK_NAMES)
-            self.army.append(vk.Viking(name= name, 
-                                       health=random.randint(80,100),
-                                       strength=random.randint(60,80)))
+        for _ in range(self.n_vikings):
+            name = random.choice(self.VIK_NAMES)
+            self.army.append(vk.Viking(name=name,
+                                       health=random.randint(80, 100),
+                                       strength=random.randint(60, 80)))
         return self.army
 
 
 class SaxonArmy:
-    
+
     def __init__(self, n_saxons) -> None:
         self.n_saxons = n_saxons
         self.army = []
 
     def create_army(self) -> list:
-        for _ in range(len(self.n_saxons)):
-            self.army.append(vk.Saxon(health=random.randint(80,100),
-                                      strength=random.randint(60,80)))
+        for _ in range(self.n_saxons):
+            self.army.append(vk.Saxon(health=random.randint(80, 100),
+                                      strength=random.randint(60, 80)))
         return self.army
-            
+
 
 class InitWar():
-    
-    def __init__(self,n_vikings,n_saxons) -> None:
+
+    def __init__(self, n_vikings, n_saxons, war=vk.War) -> None:
         self.n_vikings = n_vikings
         self.n_saxons = n_saxons
+        self.war = war
 
     def assign_army(self):
         sax = SaxonArmy(self.n_saxons)
@@ -81,13 +82,26 @@ class InitWar():
         vik_army = vik.create_army()
 
         for viking in vik_army:
-            vk.War.addViking(viking=viking)
+            self.war.addViking(viking=viking)
         for saxon in sax_army:
-            vk.War.addSaxon(saxon=saxon)
+            self.war.addSaxon(saxon=saxon)
+
+    def start_war(self):
+        # La función start_war hará turnos de 5 contiendas por ejército.
+        # Se tendrá que invocar hasta que algún ejército gane
+
+        for _ in range(5):
+            self.war.saxonAttack()
+            if self.war.showStatus == 'Saxons have fought for their lives and survive another day...':
+                return self.war.showStatus()
+            self.war.vikingAttack()
+            if self.war.showStatus == "Vikings have won the war of the century!":
+                return self.war.showStatus()
+
+        return self.war.showStatus()
 
 
-        
-                        
+batalla = InitWar(20, 20, vk.War())
 
-          
-
+batalla.assign_army()
+batalla.start_war()
